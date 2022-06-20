788d3efcce9027221a44e6151283730ae173d0bb
Rohan0095 <107842060+Rohan0095@users.noreply.github.com>
2022-06-20 14:02:14 +0530

    Update GitApplication.java

--- GithubApplication/src/Application/GitApplication.java
+++ GithubApplication/src/Application/GitApplication.java
@@ -4,6 +4,7 @@ public class GitApplication {
 	public static void Main(String[] args)
 	{
 		System.out.println("App to push in the github");
+		System.out.println("Hello this is modified by Rohan")
 	}
 
 }
